29897abfb5518a04486c30bf29dfc1f8f2bfd0a2
danid <danid@DESKTOP-63LFPTD>
2026-03-23 13:06:19 +0100

    Version 1 de Elisa

--- src/ejeHashMap/eje04/Eje_HM_4.java
+++ src/ejeHashMap/eje04/Eje_HM_4.java
@@ -1,10 +1,47 @@
 package ejeHashMap.eje04;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Eje_HM_4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		var map = new HashMap<String,String>();
+		
+		map.put("HOLA", "Hola, ¿qué tal?");
+		map.put("ENCANTADO", "Encantado de conocerte yo también");
+	    map.put("ADIOS", "Adiós, espero volverte a ver pronto");
+	    map.put("HORA", "Lo siento, no llevo reloj");
+	    map.put("NOMBRE", "Mi nombre es Eliza");
+		
+	    String msgDefault = "Lo siento, no te comprendo";
+	    
+	    Scanner sc = new Scanner(System.in); 
+	    System.out.println("Bienvenido al sistema Elizaa 0.0: ");
+	    String frase;
+	    boolean fin = false;
+	    do {
+	    	frase = sc.nextLine();
+	    	boolean encontrada = false;
+	    	for (String clave : map.keySet()) {
+	    		if(frase.toUpperCase().contains(clave)) {
+	    			encontrada = true;
+	    			System.out.println(">" + map.get(clave));
+	    			if(frase.toUpperCase().contains("ADIOS")) {
+	    				fin = true;
+	    			}
+	    			break;
+	    		} 
+				
+			}
+	    	
+	    	if(!encontrada) {
+	    		System.out.println(msgDefault);
+	    	}
+	    	
+	    } while (!fin);
+		System.out.println("Muchas gracias por conectarse");
 	}
 
 }
